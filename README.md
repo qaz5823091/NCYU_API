e8a130347fbd4729184fc7a4931f8df9e405b88a
qaz5823091 <r19991106@gmail.com>
2022-01-23 22:14:19 +0800

    update

--- README.md
+++ README.md
@@ -4,7 +4,25 @@
 ### GET `/course/{id}`  
 ### GET `/course/general`  
 ### GET `/course/department`
-### GET `/dept/{id}` 取得系所  
+* Response
+    * id
+    * select-type（選擇類別）
+    * class-type（課程類別）
+    * class-name（課程名稱）
+    * department（開課系所）
+    * system（學制）
+    * academy（學院）
+    * grade（上課年級）
+    * course-type（必選修）
+    * scores（學分數）
+    * remark（備註）
+    * teacher（教師名稱）
+    * week（上課星期）
+    * session（上課節次）
+    * classroom（教室）
+    * campus（校區）
+
+### GET `/deparments` 取得所有系所
 * Response
     * `id(string)` : 系所標號
     * `content(string array)`
@@ -12,3 +30,10 @@
         * 學制
         * 學系
 
+### GET `/department/{id}` 取得系所  
+* Response
+    * `id(string)` : 系所標號
+    * `content(string array)`
+        * 學院
+        * 學制
+        * 學系
--- app.py
+++ app.py
@@ -7,26 +7,26 @@ app.config['JSON_AS_ASCII'] = False
 with open('./department.json') as file:
     department = json.load(file)
 
-with open('./classOfDepartment.json') as file:
+with open('./1102_class_of_department.json') as file:
     class_of_department = json.load(file)
 
-with open('./classOfGeneral.json') as file:
+with open('./1102_class_of_general.json') as file:
     class_of_general = json.load(file)
 
 @app.route('/')
 def home():
     return '<h1>Welcome to NCYU API!</h1>'
 
-@app.route('/dept')
+@app.route('/departments')
 def deptAll():
     return jsonify(department)
 
-@app.route('/dept/<id>')
+@app.route('/department/<id>')
 def dept(id):
     is_exist = False
     if id != None:
         for item in department:
-            if id == item['id']:
+            if id == str(item['id']):
                 is_exist = True
                 result = item
                 break
@@ -35,6 +35,44 @@ def dept(id):
         return jsonify(item)
     else:
         return jsonify(department)
+
+@app.route('/course/departments')
+def classOfDepartment():
+    return jsonify(class_of_department)
+
+@app.route('/course/department/<id>')
+def classOfDepartmentById(id):
+    is_exist = False
+    if id != None:
+        for item in class_of_department:
+            if id == str(item['id']):
+                is_exist = True
+                result = item
+                break
+
+    if is_exist:
+        return jsonify(result)
+    else:
+        return 404
+
+@app.route('/course/generals')
+def classOfGeneral():
+    return jsonify(class_of_general)
+
+@app.route('/course/general/<id>')
+def classOfGeneralById(id):
+    is_exist = False
+    if id != None:
+        for item in class_of_general:
+            if id == item['id']:
+                is_exist = True
+                result = item
+                break
+
+    if is_exist:
+        return jsonify(result)
+    else:
+        return 404
 
 if __name__ == '__main__':
     app.run(host = 'localhost', debug = True)
